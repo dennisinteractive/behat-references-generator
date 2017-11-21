93e9800361cb1dbc953a41b163e9924536258f81
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-11-21 13:18:26 +0000

    Changed namespace

--- src/Drupal/EntityReferenceGenerator/Context/EntityReferenceGeneratorContext.php
+++ src/Drupal/EntityReferenceGenerator/Context/EntityReferenceGeneratorContext.php
@@ -1,5 +1,5 @@
 <?php
-namespace DennisDigital\Drupal\EntityReferenceGenerator\Context;
+namespace Drupal\EntityReferenceGenerator\Context;
 
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\Behat\Hook\Scope\AfterScenarioScope;
@@ -11,9 +11,11 @@ use Drupal\DrupalUserManagerInterface;
 class EntityReferenceGeneratorContext implements DrupalAwareInterface {
 
   /**
-   * @var DrupalDriverManager
+   * @inheritDoc
    */
-  private $drupal;
+  public function setDrupal(DrupalDriverManager $drupal) {
+    $this->drupal = $drupal;
+  }
 
   /**
    * @inheritDoc
@@ -29,16 +31,26 @@ class EntityReferenceGeneratorContext implements DrupalAwareInterface {
   }
 
   /**
-   * @inheritDoc
+   * @var DrupalDriverManager
    */
-  public function setDrupal(DrupalDriverManager $drupal) {
-    $this->drupal = $drupal;
-  }
+  private $drupal;
 
   /**
    * @inheritDoc
    */
   public function setDrupalParameters(array $parameters) {
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function setUserManager(DrupalUserManagerInterface $userManager) {
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getUserManager() {
   }
 
   /**
@@ -56,17 +68,5 @@ class EntityReferenceGeneratorContext implements DrupalAwareInterface {
    */
   public function afterScenario(AfterScenarioScope $scope) {
   }
-
-  /**
-   * @inheritdoc
-   */
-  public function getUserManager() {
-  };
-
-  /**
-   * @inheritdoc
-   */
-  public function setUserManager(DrupalUserManagerInterface $userManager) {
-  };
 
 }
