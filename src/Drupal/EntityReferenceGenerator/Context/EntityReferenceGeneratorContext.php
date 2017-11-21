d9d2029f1e164899694680fc47db3659b226bbd5
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-11-21 11:15:23 +0000

    Moving stuff around

---
+++ src/Drupal/EntityReferenceGenerator/Context/EntityReferenceGeneratorContext.php
@@ -0,0 +1,72 @@
+<?php
+namespace DennisDigital\Drupal\EntityReferenceGenerator\Context;
+
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
+use Behat\Testwork\Hook\HookDispatcher;
+use Drupal\DrupalDriverManager;
+use Drupal\DrupalExtension\Context\DrupalAwareInterface;
+use Drupal\DrupalUserManagerInterface;
+
+class EntityReferenceGenerator implements DrupalAwareInterface {
+
+  /**
+   * @var DrupalDriverManager
+   */
+  private $drupal;
+
+  /**
+   * @inheritDoc
+   */
+  public function setDispatcher(HookDispatcher $dispatcher) {
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getDrupal() {
+    return $this->drupal;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function setDrupal(DrupalDriverManager $drupal) {
+    $this->drupal = $drupal;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function setDrupalParameters(array $parameters) {
+  }
+
+  /**
+   * @BeforeScenario
+   *
+   * @param BeforeScenarioScope $scope
+   */
+  public function beforeScenario(BeforeScenarioScope $scope) {
+  }
+
+  /**
+   * @AfterScenario
+   *
+   * @param AfterScenarioScope $scope
+   */
+  public function afterScenario(AfterScenarioScope $scope) {
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getUserManager() {
+  };
+
+  /**
+   * @inheritdoc
+   */
+  public function setUserManager(DrupalUserManagerInterface $userManager) {
+  };
+
+}
